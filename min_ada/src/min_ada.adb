597df50028f9e1cec03087b1c878d2434d3a9601
Florian Koudjonou <floriankoud@hotmail.com>
2023-10-24 22:17:46 -0400

    ADASCOPE-124 WIP

--- min_ada/src/min_ada.adb
+++ min_ada/src/min_ada.adb
@@ -10,4 +10,22 @@ package body Min_Ada is
    begin
       Ada.Text_IO.Put_Line ("Hello");
    end Send_Frame;
+
+   procedure Rx_Bytes (
+      Context   : in out Min_Context;
+      Data      : Byte
+   ) is
+   begin
+      if Context.Rx_Header_Bytes_Seen = 2 then
+         Context.Rx_Header_Bytes_Seen := 0;
+
+         if Data = HEADER_BYTE then
+            Context.Rx_Frame_State := RECEIVING_ID_CONTROL;
+            return;
+
+         elsif Data = STUFF_BYTE then
+            return;
+         end if;
+      end if;
+   end Rx_Bytes;
 end Min_Ada;
--- min_ada/src/min_ada.ads
+++ min_ada/src/min_ada.ads
@@ -2,8 +2,24 @@ package Min_Ada is
 
    type Bit     is mod 2**1     with Size => 1;
    type Byte    is mod 2**8     with Size => 8;
+   type UInt4   is mod 2**4     with Size => 4;
    type UInt8   is mod 2**8     with Size => 8;
    type UInt32  is mod 2**32    with Size => 32;
+
+   HEADER_BYTE           : constant Byte := 16#AA#;
+   STUFF_BYTE            : constant Byte := 16#55#;
+   EOF_BYTE              : constant Byte := 16#55#;
+
+   SEARCHING_FOR_SOF     : constant UInt4 := 0;
+   RECEIVING_ID_CONTROL  : constant UInt4 := 1;
+   RECEIVING_SEQ         : constant UInt4 := 2;
+   RECEIVING_LENGTH      : constant UInt4 := 3;
+   RECEIVING_PAYLOAD     : constant UInt4 := 4;
+   RECEIVING_CHECKSUM_3  : constant UInt4 := 5;
+   RECEIVING_CHECKSUM_2  : constant UInt4 := 6;
+   RECEIVING_CHECKSUM_1  : constant UInt4 := 7;
+   RECEIVING_CHECKSUM_0  : constant UInt4 := 8;
+   RECEIVING_EOF         : constant UInt4 := 9;
 
    type App_ID is mod 2**6
       with Size => 6;
@@ -37,10 +53,10 @@ package Min_Ada is
       Tx_Checksum               : Crc32_Context;
          --  Calculated checksum for sending frame
 
-      Rx_header_bytes_seen      : UInt8;
+      Rx_Header_Bytes_Seen      : UInt8;
          --  Countdown of header bytes to reset state
 
-      Rx_frame_state            : UInt8;
+      Rx_Frame_State            : UInt4;
          --  State of receiver
 
       Rx_Frame_Payload_Bytes    : UInt8;
@@ -70,6 +86,11 @@ package Min_Ada is
       ID                : App_ID;
       Payload           : Min_Payload;
       Payload_Length    : UInt8
+   );
+
+   procedure Rx_Bytes (
+      Context   : in out Min_Context;
+      Data      : Byte
    );
 
 end Min_Ada;
